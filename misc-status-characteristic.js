4c3878249c6213f2b840f3740515e1ad9bc8bb8c
Henrik Larsson <laselase@gmail.com>
2017-03-17 19:30:53 +0100

    add status write

--- misc-status-characteristic.js
+++ misc-status-characteristic.js
@@ -11,7 +11,7 @@ var MiscStatusCharacteristic = function() {
 
   MiscStatusCharacteristic.super_.call(this, {
     uuid: 'FB880903-4AB2-40A2-A8F0-14CC1C2E5608',
-    properties: ['read'],
+    properties: ['read', 'write'],
     descriptors: [
       // User description
       new Descriptor({
@@ -51,6 +51,33 @@ MiscStatusCharacteristic.prototype.onReadRequest = function(offset, callback) {
     } else {
       var buf = this.statusMsg.slice(offset);
       callback(thisObj.RESULT_SUCCESS, buf);
+    }
+  }
+};
+
+
+MiscStatusCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+  console.log('MiscStatusCharacteristic - onWriteRequest');
+  var thisObj = this;
+  var offsetToUseStr = data.toString('utf-8');
+  var offsetToUse = parseInt(offsetToUseStr);
+  if (offsetToUse == 0) {
+    httphelper.getStatus(function (status, statusMsg) {
+      console.log('MiscStatusCharacteristic - onWriteRequest: status = "' + status + '" value = ' + (statusMsg != null ? statusMsg : 'null'));
+      thisObj.statusMsg = new Buffer(statusMsg, "utf-8");
+      if (status == 'OK') {
+        callback(thisObj.RESULT_SUCCESS);
+      } else {
+        callback(thisObj.RESULT_UNLIKELY_ERROR);
+      }
+    });
+  } else {
+    if (offsetToUse <= this.statusMsg.length) {
+      this.statusMsg = this.statusMsg.slice(offsetToUse);
+      callback(thisObj.RESULT_SUCCESS);
+    } else {
+      this.statusMsg = null;
+      callback(thisObj.RESULT_UNLIKELY_ERROR);
     }
   }
 };
